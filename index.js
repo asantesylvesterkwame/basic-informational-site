62f56185b395ca31902a662d21e01fd8c265af33
asantesylvesterkwame <intelligentson17@gmail.com>
2023-03-12 02:11:14 +0000

    created routes for my html files

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var fs = require('fs')
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+ const hostname = '127.0.0.1';
+const port = 8080;
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// url variable
+
+const server = http.createServer(function (req,res) {
+    console.log('the link that was requested is '+ " " +req.url );
+    if (req.url === '/home' || req.url === '/') {
+        res.writeHead(200,{"Content-Type": "text/html"});
+        fs.createReadStream(__dirname+'/index.html').pipe(res);
+
+    }
+    else if(req.url === '/about'){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        fs.createReadStream(__dirname + '/about.html').pipe(res);
+    }
+    else if(req.url === '/contact-me'){
+        res.writeHead(200, {"Content-Type":"text/html"});
+        fs.createReadStream(__dirname + '/contact-me.html').pipe(res)
+    }
+    else{
+        res.writeHead(404, {"Content-Type": "text/html"});
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+    }
+    console.log("server is running");
+}).listen(port,hostname)
+
+
+
